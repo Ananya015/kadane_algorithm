b2d4c6b1d89f89358d1300a1f00779c261f4e9f0
Ananya015 <jainanaya78@gla.ac.in>
2020-05-09 01:04:10 +0530

    initialize array

--- src/algorithm/Kadane.java
+++ src/algorithm/Kadane.java
@@ -8,7 +8,9 @@ public class Kadane {
         System.out.println ("Enter size of array");
         int size=sc.nextInt();
         int[] arr=new int[size];
-
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt ();
+        }
     }
 
 }
